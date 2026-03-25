84afc1c96a9bee5e3a6730852a49778ed0bb3ada
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-25 21:49:18 +0530

    onboarding_screen.dart: OnboardingDataWidget refactor.

--- lib/onboarding/screen/onboarding_screen.dart
+++ lib/onboarding/screen/onboarding_screen.dart
@@ -19,61 +19,18 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            /// Skip Button Widget
+            SkipButtonWidget(onSkipPress: () {}),
             const SizedBox(height: 50),
 
-            /// Skip Button
-            SkipButtonWidget(onSkipPress: () {}),
-            const SizedBox(height: 20),
-
             /// Onboarding Data
-            Expanded(
-              child: PageView.builder(
-                controller: _pageCtrl,
-                onPageChanged: (index) {},
-                itemCount: onboardingData.length,
-                itemBuilder: (ctx, index) {
-                  return Column(
-                    children: [
-                      /// Icon
-                      Image(
-                        image: AssetImage(onboardingData[index].icon),
-                        height: 180,
-                        width: 180,
-                      ),
-                      const SizedBox(height: 50),
-
-                      /// Title
-                      Text(
-                        AppTexts.kOnBoardingTitle1,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.purple.shade400,
-                          letterSpacing: 1,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-
-                      /// Description
-                      Text(
-                        AppTexts.kOnBoardingDesc1,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.purple.shade200,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w300,
-                          letterSpacing: 1.5,
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
+            OnboardingDataWidget(
+              pageCtrl: _pageCtrl,
+              onPageChanged: (index) {},
             ),
 
             /// Dot Indicator
@@ -83,6 +40,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               onDotClicked: (index) {},
               effect: JumpingDotEffect(verticalOffset: 2),
             ),
+            SizedBox(height: 150.0),
 
             /// Next Button
             SizedBox(
@@ -94,6 +52,64 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class OnboardingDataWidget extends StatelessWidget {
+  const OnboardingDataWidget({
+    super.key,
+    required PageController pageCtrl,
+    required this.onPageChanged,
+  }) : _pageCtrl = pageCtrl;
+
+  final PageController _pageCtrl;
+  final Function(int) onPageChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: PageView.builder(
+        controller: _pageCtrl,
+        onPageChanged: onPageChanged,
+        itemCount: onboardingData.length,
+        itemBuilder: (ctx, index) {
+          // Filter Single Item
+          final item = onboardingData[index];
+          return Column(
+            children: [
+              /// Icon
+              Image(image: AssetImage(item.icon), height: 200, width: 200),
+              const SizedBox(height: 50),
+
+              /// Title
+              Text(
+                item.title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.purple.shade400,
+                  letterSpacing: 1,
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              /// Description
+              Text(
+                item.desc,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.purple.shade200,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: 1.5,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
