3acbf9b12fdc475a09e24406311378e3c08d55a4
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-25 20:58:24 +0530

    app_icons.dart: .json converts to png.

--- lib/utils/constants/app_icons.dart
+++ lib/utils/constants/app_icons.dart
@@ -2,8 +2,8 @@ class AppIcons {
   AppIcons._();
 
   /// Onboarding Icons
-  static const String kOnboarding_icon1 = "assets/onboarding/onboarding_1.json";
-  static const String kOnboarding_icon2 = "assets/onboarding/onboarding_2.json";
-  static const String kOnboarding_icon3 = "assets/onboarding/onboarding_3.json";
-  static const String kOnboarding_icon4 = "assets/onboarding/onboarding_4.json";
+  static const String kOnboarding_icon1 = "assets/onboarding/onboarding_1.png";
+  static const String kOnboarding_icon2 = "assets/onboarding/onboarding_2.png";
+  static const String kOnboarding_icon3 = "assets/onboarding/onboarding_3.png";
+  static const String kOnboarding_icon4 = "assets/onboarding/onboarding_4.png";
 }
